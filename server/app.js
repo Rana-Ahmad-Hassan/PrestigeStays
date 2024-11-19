f4e76260283166b66b8f2117469f02ad6aa59b96
Rana-Ahmad-Hassan <r80228927@gmail.com>
2024-11-19 18:25:04 +0500

    changes

--- server/app.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-
-
-export const app = express();
-app.use(express.json({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
