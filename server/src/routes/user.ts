0a691a3de88f97a8d6115388bec5602a619fd1bf
Rana-Ahmad-Hassan <r80228927@gmail.com>
2024-11-20 19:07:31 +0500

    zod user validation

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,8 +1,9 @@
 import { signUp } from "@/controllers/user";
 import { Router } from "express";
+import { validateHeaderValue } from "http";
 
 const userRouter = Router();
 
-userRouter.post("/signup", signUp);
+userRouter.post("/signup", validateHeaderValue, signUp);
 
 export default userRouter;
